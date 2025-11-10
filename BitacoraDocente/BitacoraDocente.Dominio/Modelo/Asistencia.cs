dcab946d2de92955783292656c11999a478e8c9f
Gerard4oo2 <gerardxd4002@gmail.com>
2025-11-09 23:57:41 -0500

    agregar entidad Desempenio y Asistencia

---
+++ BitacoraDocente/BitacoraDocente.Dominio/Modelo/Asistencia.cs
@@ -0,0 +1,28 @@
+﻿using BitacoraDocente.Dominio.Utilidades;
+using BitacoraDocente.Intercambio.Dto.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitacoraDocente.Dominio.Modelo
+{
+    public class Asistencia
+    {
+        public int AsistenciaId { get; set; }
+        public int EstudianteId { get; set; } 
+        public int SesionId { get; set; }   
+        public int FechaId { get; set; }     
+
+        public string Observacion { get; set; } = string.Empty;
+        public bool Estado { get; set; }
+
+        public virtual Estudiante Estudiante { get; set; } = new Estudiante();
+        public virtual ICollection<Asistencia> Asistencia { get; set; } = new List<Asistencia>();
+
+        public ResultadoOperacion<bool> Validar()
+        {
+            var respuesta = EntidadValidador.ValidarCamposRequeridos(this);
+            return respuesta;
+        }
---
+++ BitacoraDocente/BitacoraDocente.Dominio/Modelo/Desempenio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitacoraDocente.Dominio.Utilidades;
+using BitacoraDocente.Intercambio.Dto.General;
+using System;
+
+namespace BitacoraDocente.Dominio.Modelo
+{
+    public class Desempenio
+    {
+        public int DesempenioId { get; set; }
+
+        // Foráneas
+        public int CapacidadId { get; set; }   
+        public int EstudianteId { get; set; }  
+        public int SesionId { get; set; }     
+
+        // Estado y fecha
+        public bool EstaLogrado { get; set; }
+        public DateTime FechaRegistro { get; set; } 
+
+        public virtual Estudiante Estudiante { get; set; } = new Estudiante();
+        public virtual Capacidad Capacidad { get; set; } = new Capacidad();
+        public virtual Sesion Sesion { get; set; } = new Sesion();
+
+
+        public ResultadoOperacion<bool> Validar()
+        {
+            var respuesta = EntidadValidador.ValidarCamposRequeridos(this);
+            return respuesta;
+        }
+    }
+}
