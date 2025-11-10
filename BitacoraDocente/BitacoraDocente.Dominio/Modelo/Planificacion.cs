0a3c26c7b10e4d98aac30f6d70d4738bb3d17af7
Astronomical Developer <astronomical.developer@gmail.com>
2025-11-09 23:46:23 -0500

    Modelo de entidad AreaCurricular y Planificacion

--- BitacoraDocente/BitacoraDocente.Dominio/Modelo/Planificacion.cs
+++ BitacoraDocente/BitacoraDocente.Dominio/Modelo/Planificacion.cs
@@ -11,12 +11,14 @@ namespace BitacoraDocente.Dominio.Modelo
     public class Planificacion
     {
         public int PlanificacionId { get; set; }
-        public DateOnly AnioEscolar { get; set; }
         public int AreaCurricularId { get; set; }
+        public DateOnly AnioEscolar { get; set; }
         public string Proposito { get; set; } = string.Empty;
         public DateTime FechaCreacion { get; set; }
         public string SeccionGrado { get; set; } = string.Empty;
+
         public virtual AreaCurricular AreaCurricular { get; set; } = new AreaCurricular();
+        public virtual ICollection<UnidadDidactica> { get; set; } = new List<UnidadDidactica>();
 
         public ResultadoOperacion<bool> Validar()
         {
